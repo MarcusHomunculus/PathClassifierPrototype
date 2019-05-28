8258f584b83b3b47f80b9a7bd144a52a1f2e7518
Marcus Homunculus <marcus.homunculus@posteo.net>
2019-05-28 11:23:31 +0200

    Implemented in main a workers-to-json program fragment

--- main.py
+++ main.py
@@ -2,4 +2,12 @@
 The main routine
 """
 
-print("Hello World")
+from creation import WorkerCreator
+
+
+wc = WorkerCreator.WorkerCreator(3)
+result_file = open("workers.json", "w")
+result_file.write(wc.to_json())
+result_file.close()
+
+
