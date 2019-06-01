807f2f9e4e4289dc020c7ed323876c3bd103ae56
Marcus Homunculus <marcus.homunculus@posteo.net>
2019-06-01 14:51:11 +0200

    Restructured main script to better reflect the intention where it should be going -> inserted first dialog for generating json

--- main.py
+++ main.py
@@ -2,12 +2,38 @@
 The main routine
 """
 
-from creation import WorkerCreator
+from creation import WorkerCreator, SectionCreator
+
+workerFile = "workers.json"
+sectionFile = "sections.json"
 
 
-wc = WorkerCreator.WorkerCreator(3)
-result_file = open("workers.json", "w")
-result_file.write(wc.to_json())
-result_file.close()
+def generate_json() -> None:
+    """
+    A wrapper function to wrap the json generation in one function call
+    """
+    wc = WorkerCreator.WorkerCreator(3)
+    result_file = open(workerFile, "w")
+    result_file.write(wc.to_json())
+    result_file.close()
+
+    sc = SectionCreator.SectionCreator()
+    result_file = open(sectionFile, "w")
+    result_file.write(sc.to_json())
+    result_file.close()
 
 
+print("Step one: creating environment:")
+generateBase = input("Do you want me to generate data for you? (y/n)")
+if generateBase == "y" or generateBase == "Y":
+    # means to generate the json files from stock
+    generate_json()
+elif generateBase == "n" or generateBase == "N":
+    # pass here to cover all other inputs with the else branch
+    pass
+else:
+    raise AttributeError("Could not map %s to a 'y' or 'n'".format(generateBase))
+
+# TODO: continue here
+
+print("Done!")
