a939238e068ebbb0ea2d465a168385e549f932f3
Marcus Homunculs <marcus.homunculus@posteo.net>
2019-09-20 20:50:14 +0200

    Generalized the the function for dumping the workers into the xlsx-table, to dump the sections list (in a non beatified manner), too

--- creation/Creator.py
+++ creation/Creator.py
@@ -81,11 +81,13 @@ class Creator:
         wb = Workbook()
         # a blank sheet is created by default: use this instead of creating a new one
         current = wb.active
+        current.title = "Workers"
         # foreach property of worker
-        self._create_worker_table(current, 4, 2, "Workers")
-        wb.save(file_name)
+        self._create_single_table(current, self.__workerList, 4, 2, "Worker")
         # create the sheet with the sections
-        wb.create_sheet("Sections")
+        current = wb.create_sheet("Sections")
+        self._create_single_table(current, self.__sectionList, 4, 2, "Sections")
+        wb.save(file_name)
         # create the sheet where to every section a worker is assigned
         wb.create_sheet("Assignments")
 
@@ -100,19 +102,19 @@ class Creator:
         """
         return len(self.__sectionList) != 0 and len(self.__workerList) != 0
        
-    def _create_worker_table(self, workbook, start_row: int, start_column: int, name: str, offset_row: int = 2, offset_col: int = 2):
-        workbook.title = name
+    def _create_single_table(self, workbook, data: List[_DataStruct], start_row: int, start_column: int, name: str, offset_row: int = 2, offset_col: int = 0):
         workbook.cell(row=start_row, column=start_column).value = name
         row_range = range(len(self.__workerList))
-        # use the first worker as template
-        worker_keys = self.__workerList[0].attributes.keys()
+        # use the first data point as template
+        # self.__workerList[0].attributes.keys()
+        header_keys = data[0].attributes.keys()
         # create the table header
         header_fill = PatternFill(start_color='FFFFFF00',
             end_color='FFFFFF00', fill_type='solid')
         header_alignment = Alignment(horizontal='center')
         table_row = start_row + offset_row
         current_col = start_column + offset_col
-        for key in worker_keys:
+        for key in header_keys:
             current_cell = workbook.cell(row=table_row, column=current_col)
             current_cell.value = str(key)
             # do some styling
@@ -122,17 +124,20 @@ class Creator:
             col_letter = get_column_letter(current_col)
             # use the first worker as a template again
             # take whatever is longer: key or content
-            content_width = len(str(self.__workerList[0].attributes[key]))
+            content_width = len(str(data[0].attributes[key]))
             key_width = len(key)
             width = key_width if key_width > content_width else content_width
             workbook.column_dimensions[col_letter].width = 1.8 * width
             current_col += 1
         for y in row_range:
-            current_col = start_column
-            for key in worker_keys:
+            current_col = start_column + offset_col
+            for key in header_keys:
                 cell = workbook.cell(row=table_row + 1 + y, column=current_col)
-                cell.value = str(self.__workerList[y].attributes[key])
+                cell.value = str(data[y].attributes[key])
                 current_col += 1
+    
+    def _create_cross_table(self, workbook, start_row: int, start_column: int):
+        pass
 
 if __name__ == "__main__":
     c = Creator()
