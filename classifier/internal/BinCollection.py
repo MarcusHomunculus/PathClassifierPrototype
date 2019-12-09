f763697e7155f682054c77f0a48773b5856c2dfd
Marcus Homunculus <marcus.homunuclus@posteo.net>
2019-12-09 11:07:34 +0100

    Started with implementing an approach on storing a match count

---
+++ classifier/internal/BinCollection.py
@@ -0,0 +1,48 @@
+from typing import List
+import operator
+
+
+class _BinCollection:
+
+    __source_path: str
+    __matched_paths: List[str]
+    __match_bins: List[int]
+    __last_match: str
+
+    def __init__(self, source_path: str):
+        self.__source_path = source_path
+        self.__matched_paths = []
+        self.__match_bins = []
+        self.__last_match = ""
+
+    def add_matched_path(self, possible_match_path: str) -> None:
+        # TODO: doc me
+        def match_index(to_find: str) -> int:
+            # TODO: write some nice docu here
+            for i in range(len(self.__matched_paths)):
+                if self.__matched_paths[i] == to_find:
+                    return i
+            return -1
+
+        idx = match_index(possible_match_path)
+        if idx == -1:
+            self.__matched_paths.append(possible_match_path)
+            idx = len(self.__matched_paths) - 1
+        if len(self.__match_bins) < idx:
+            self.__match_bins.append(0)
+        self.__match_bins[idx] += 1
+
+    def get_highest_match(self) -> (str, bool):
+        # TODO: doc me
+        max_val_is_unique = True
+        max_val = -1    # this value is impossible with the bins
+        max_idx = 0
+        for i in range(len(self.__match_bins)):
+            if self.__match_bins[i] > max_val:
+                max_val = self.__match_bins[i]
+                max_idx = i
+                max_val_is_unique = True
+            elif self.__match_bins[i] == max_val:
+                # uniqueness is not given right now
+                max_val_is_unique = False
+        return self.__matched_paths[max_idx], max_val_is_unique
