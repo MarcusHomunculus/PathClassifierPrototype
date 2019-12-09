2d3fde5769ade4983f43ef95352b1ca1a45bc147
Marcus Homunculus <marcus.homunuclus@posteo.net>
2019-12-09 10:33:36 +0100

    Started with developing the classifier API

---
+++ classifier/BinClassifier.py
@@ -0,0 +1,14 @@
+from classifier.BinMatrix import BinMatrix
+
+
+class BinClassifier:
+
+    #__mat: BinMatrix
+    __last_source: str
+
+    def __init__(self):
+        #self.__mat = BinMatrix()
+        self.__last_source = None
+
+    def add_source_path(self, source: str):
+        pass
