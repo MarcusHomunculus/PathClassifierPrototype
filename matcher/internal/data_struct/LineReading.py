b248e24f06edd4dae19bc978cc980c1536e570b8
Marcus Homunculus <marcus.homunuclus@posteo.net>
2020-01-06 22:02:43 +0100

    Added docu and boolean getter for the forwarding information in LineResultStruct

--- matcher/internal/data_struct/LineReading.py
+++ matcher/internal/data_struct/LineReading.py
@@ -32,13 +32,23 @@ class LineResultStruct:
 
     @staticmethod
     def create_no_find() -> LineResultStruct:
-        # TODO: doc me
+        """
+        Creates an empty instance which represents the information that nothing of use could be found
+
+        :return an instance representing nothing
+        """
         return LineResultStruct(LineResultType.NO_FINDING, CellMatchingStruct([]), CellPosition.create_invalid(),
                                 CellPosition.create_invalid())
 
     @staticmethod
     def create_header_found(forward_position: CellPosition) -> LineResultStruct:
-        # TODO: write some expressive docu here
+        """
+        Creates an instance which represents that a header was detected and at which position the forwarding content
+        could be matched
+
+        :param forward_position: the position of the forwarding column or row
+        :return: an instance representing a header (which in itself) is only of importance for were to start reading
+        """
         # instantiate an empty matching struct with no list -> use the name member to transport the forward index
         return LineResultStruct(LineResultType.HEADER_FOUND, CellMatchingStruct([]), CellPosition.create_invalid(),
                                 forward_position)
@@ -47,5 +57,20 @@ class LineResultStruct:
     def create_data_found(match_result: CellMatchingStruct,
                           value_position: CellPosition,
                           name_position: CellPosition) -> LineResultStruct:
-        # TODO: I need some docu here
+        """
+        Creates an instance which holds the position of the data found
+
+        :param match_result: the struct holding the match information
+        :param value_position: the position of the value in the table
+        :param name_position: the position of the name in the table
+        :return: an instance representing an success in finding a value name pair in the given line
+        """
         return LineResultStruct(LineResultType.DATA_FOUND, match_result, value_position, name_position)
+
+    def contains_forwarding(self) -> bool:
+        """
+        Answers if the struct represents a header which holds a valid position for a header row or column
+
+        :return: true if its an instance of a header which has forwarding information
+        """
+        return self.read_result == LineResultType.HEADER_FOUND and self.name_or_forward_position.is_valid()
