317ae3e0a482f34ba39ac2ec32fa7d6e03daee60
Marcus Homunculus <marcus.homunuclus@posteo.net>
2020-01-12 11:50:55 +0100

    WIP: continued implementing the HTML-writer

--- matcher/internal/debug/HtmlWriter.py
+++ matcher/internal/debug/HtmlWriter.py
@@ -3,9 +3,9 @@ import html
 
 class HtmlWriter:
 
-    _raw_data: List[Tuple[str, List[str, int]]]
+    _raw_data: List[Tuple[str, List[Tuple[str, int]]]]
 
-    def __init__(self, raw_data: List[Tuple[str, List[str, int]]]):
+    def __init__(self, raw_data: List[Tuple[str, List[Tuple[str, int]]]]):
         """
         The constructor
 
@@ -21,15 +21,15 @@ class HtmlWriter:
                 return template_file.read()
         # retrieve a list of all paths registered from the sink file
         sink_path_set = set()
-        for line in self.__mat:
-            sink_path_set.update(line.get_potential_paths())
+        for line in self._raw_data:
+            sink_path_set.update(line[1][0])
         # transform it to a list to ensure order
-        sink_paths = list(sink_path_set)
+        sink_path_pool = list(sink_path_set)
         content = get_template("match_table")
         # creating the table header
         header_template = get_template("table_head")
         head_string = ""
-        for sink_path in sink_paths:
+        for sink_path in sink_path_pool:
             head_string += header_template.replace("[%PATH%]", html.escape(sink_path)) + "\n"
         # insert the table head
         content = content.replace("[%SINK_PATHS%]", head_string, 1)
@@ -37,8 +37,8 @@ class HtmlWriter:
         row_template = get_template("table_row")
         cell_template = get_template("match_cell")
         table_body = ""
-        for row in self.__mat:
-            current_row = row_template.replace("[%SOURCE_PATH%]", row.get_key())
+        for row in self._raw_data:
+            current_row = row_template.replace("[%SOURCE_PATH%]", row[0])
             matrix_row_str = ""
             for bin_count in row.get_bins():
                 matrix_row_str += cell_template.replace("[%COUNT%]", str(bin_count)) + "\n"
@@ -50,3 +50,28 @@ class HtmlWriter:
         print("Dumping classifier matrix as HTML into " + target_path)
         with open(target_path, "w+") as sink_file:
             sink_file.write(content)
+
+    def __get_bin_count_for(self, source_path: str, sink_path: str) -> int:
+        """
+        Returns the number of increments for the given source-path-to-sink-path-registrations
+
+        :param source_path: the path from the source file
+        :param sink_path: the path to the sink path in question
+        :return: the count of registrations for the given constellation
+        """
+        def get_pair_list(key: str) -> List[Tuple[str, int]]:
+            """
+            Extracts the list of path-count-pairs which belong to the given source path
+
+            :param key: the source path to use as key
+            :return: the list of registered paths and their count to the given key (-path)
+            """
+            for entry in self._raw_data:
+                if entry[0] == key:
+                    return entry[1]
+        pair_list = get_pair_list(source_path)
+        for pair in pair_list:
+            if pair[0] == sink_path:
+                return pair[1]
+        # if the sink_path hadn't been found it was never registered ergo it is zero
+        return 0
