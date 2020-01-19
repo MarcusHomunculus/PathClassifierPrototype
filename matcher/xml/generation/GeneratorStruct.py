1911abc8737618bf24d1233fa447baf8bbf4bbd2
Marcus Homunculus <marcus.homunuclus@posteo.net>
2020-01-19 17:22:49 +0100

    WIP: continued implementing the helper struct for the generation: made a 1st incomplete draft for the function to add a path to the existing list

--- matcher/xml/generation/GeneratorStruct.py
+++ matcher/xml/generation/GeneratorStruct.py
@@ -1,62 +1,80 @@
-from typing import List, Tuple
-import re
+from typing import List, Tuple, Set
+
+from matcher.path.PathOperations import PathOperator
 
 
 class GeneratorStruct:
+
     root_path: str
     name_path: str
-    node_paths: List[str]
+    node_paths: List[str]   # do not use a set here to ensure ordering
 
-    def __init__(self, path_collection: List[str]):
+    def __init__(self, name_path: str):
         # TODO: write some docu here -> recommend not to use it
         # the structure is kinda standardized: the 1st node is the general group node, the 2nd is the root node for
         # every "thing" -> as all of them have to have them just pick the first
-        self.__root_path = GeneratorStruct.extract_base_path(path_collection[0])
+        self.root_path = PathOperator.extract_base_path(name_path)
+        self.name_path = name_path
+        self.node_paths = []
+
+    def matches_jurisdiction(self, path_to_check: str) -> bool:
+        """
+        Checks if the given path falls into the base class / base path this instance represents
+
+        :param path_to_check: the path to check for applicability to this instance
+        :return: if the instance class and the path fall into the class or not
+        """
+        return self.root_path in path_to_check
+
+    def add_path(self, to_add: str) -> bool:
+        # TODO: your docu could stand right here
+        if not self.matches_jurisdiction(to_add):
+            return False
+        index = len(self.node_paths)        # default to append the path
+        if PathOperator.is_nodes_only(to_add):
+            # some attribute paths might be added before their parent path -> check
+            result = self.__get_index_of_attribute_path(to_add)
+            if result != -1 and self.node_paths:    # also check if the list is not empty
+                index = result - 1      # set it before the attribute path
+            # else leave the index at the default
+        else:
+            # means it points to an attribute -> check if the "parent" path is available and put it behind it
+            result = self.__get_index_of_attribute_parent(to_add)
+            if result != -1:
+                index = result + 1  # add one to put it behind its parent
+        # if now node-attribute assignment has been performed check if grouping by nested "classes" makes sense
+        if index == len(self.node_paths) and PathOperator.contains_iterations(to_add):
+            pass
+        # TODO: do some ordering here -> get the required index
+        self.node_paths.insert(index, to_add)
+        return True
 
     @staticmethod
-    def construct_from(path_collection: List[str]) -> List[str]:
+    def construct_from(name_paths: Set[str], path_collection: List[str]) -> List[str]:
+        # TODO: I need some docu here
         pass
 
-    @staticmethod
-    def get_node_count(node_path: str) -> int:
+    def __get_index_of_attribute_parent(self, to_find_parent_of: str) -> int:
         """
-        Counts the nodes the path addresses excluding the last attribute if present
+        Checks if the given attribute path already has a parent path in the list of stored paths
 
-        :param node_path: the path to count the node of
-        :return: the count of actual nodes in the path
+        :param to_find_parent_of: the path to which the node itself is required
+        :return: the index of the parent node and -1 if no parent could be found
         """
-        # as intermediate attributes and path changes are not to be expected
-        path_parts = node_path.split("/")
-        if path_parts[-1].startswith("@"):
-            return len(path_parts) - 1
-        return len(path_parts)
+        for i in range(len(self.node_paths)):
+            if PathOperator.is_attribute_path_of(to_find_parent_of, self.node_paths[i]):
+                return i
+        return -1
 
-    @staticmethod
-    def is_nodes_only(node_path: str) -> bool:
+    def __get_index_of_attribute_path(self, to_find_attribute_path_of: str) -> int:
         """
-        Returns if the node contains only nodes or if attributes are included
+        Checks if the given path might be a parent of an existing attribute path and returns the first index of this
+        attribute addressing path if this is the case
 
-        :param node_path: the path to check
-        :return: if the last path element is an attribute or not
+        :param to_find_attribute_path_of: the index of a path that addresses an attribute of the given path
+        :return: the index of the first attribute path to the given path
         """
-        # regex checks if a '@' is in front of the very last word
-        return bool(re.match(r"@\w+$", node_path))
-
-    @staticmethod
-    def extract_base_path(to_extract_from) -> str:
-        """
-        Extracts the base path from the given path which is the first 2 nodes only
-
-        :param to_extract_from: the path which holds the wanted information
-        :return: the path of the 2 first nodes
-        """
-        try:
-            # regex matches the first 2 words separated by a '/'
-            return re.search(r"^\w+/\w+\b", to_extract_from).group(1)
-        except AttributeError:
-            raise AttributeError("Path seems to violate the path structure: {}".format(to_extract_from))
-
-    @staticmethod
-    def __paths_match_up_to(to_check: str, to_check_against: str) -> Tuple[int, int]:
-        # TODO: doc me -> first is how far they match; 2nd is how much "difference" is left
-        pass
+        for i in range(len(self.node_paths)):
+            if PathOperator.is_attribute_path_of(self.node_paths[i], to_find_attribute_path_of):
+                return i
+        return -1
