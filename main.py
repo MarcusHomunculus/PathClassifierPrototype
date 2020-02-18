c72d9f75cc2541c03e625ffff2ac49a6eac704e5
Marcus Homunculus <marcus.homunuclus@posteo.net>
2020-02-18 15:25:34 +0100

    "Activated" the UI for the script in main

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from creation import WorkerCreator, SectionCreator, Creator, FileModifier
 from matcher.MatchingManager import MatchingManager
 from evaluation.Differ import XmlDiffer
 
-workerFile = "workers.json"
-sectionFile = "sections.json"
+WORKER_FILE = "workers.json"
+SECTION_FILE = "sections.json"
 
 
 def generate_json(worker_count: int) -> None:
@@ -17,14 +17,27 @@ def generate_json(worker_count: int) -> None:
     A wrapper function to wrap the json generation in one function call
     """
     wc = WorkerCreator.WorkerCreator(worker_count)
-    result_file = open(workerFile, "w")
+    result_file = open(WORKER_FILE, "w")
     result_file.write(wc.to_json())
     result_file.close()
 
     sc = SectionCreator.SectionCreator()
-    result_file = open(sectionFile, "w")
+    result_file = open(SECTION_FILE, "w")
     result_file.write(sc.to_json())
     result_file.close()
+
+
+def try_parse_int(to_parse: str) -> (bool, int):
+    """
+    Returns in the first boolean if the int in the string could be parsed and in the second the parsed int
+
+    :param to_parse: the string which shall be parsed for an integer
+    :return: a tuple containing the success-state of the parsing and the int if successful else 0
+    """
+    try:
+        return True, int(to_parse)
+    except ValueError:
+        return False, 0
 
 
 if __name__ == "__main__":
@@ -33,31 +46,40 @@ if __name__ == "__main__":
     default_xml_path = "ref.xml"
     default_target_file = "test.xml"
     default_config_path = "config.toml"
-    worker_cnt = 20
+    default_worker_cnt = 20
     print("Step one: creating environment:")
-    # shall_generate_base = input("Do you want me to generate data for you? (y/n)")
-    shall_generate_base = "y"
+    previous_step_performed: bool = True
+    shall_generate_base = input("Do you want me to generate data for you? (y/n)")
     if shall_generate_base == "y" or shall_generate_base == "Y":
+        worker_count_input = input("How many workers shall be generated? (Default: {})".format(default_worker_cnt))
+        not_default, worker_count = try_parse_int(worker_count_input)
+        if not not_default:
+            print("Did not understood the input. Using default value {}".format(default_worker_cnt))
+            worker_count = default_worker_cnt
         # means to generate the json files from stock
         # worker_cnt = int(input("How many workers should be generated?"))
-        generate_json(worker_cnt)
+        generate_json(worker_count)
     elif shall_generate_base == "n" or shall_generate_base == "N":
+        previous_step_performed = False
         # pass here to cover all other inputs with the else branch
         pass
     else:
-        raise AttributeError("Could not map %s to a 'y' or 'n'".format(shall_generate_base))
+        raise AttributeError("Could not map {} to a 'y' or 'n'".format(shall_generate_base))
 
     # print an empty line for separation
     print()
     print("Step two: Generating mock data from JSON")
-    # shall_generate_mock = input("Shall I use the existing data to generate mocking data? (y/n)")
-    shall_generate_mock = "y"
+    if previous_step_performed:
+        shall_generate_mock = input("Shall I use the existing data to generate mocking data? (y/n)")
+    else:
+        shall_generate_mock = "n"
     if shall_generate_mock == "y" or shall_generate_mock == "Y":
-        c = Creator.Creator(path_to_workers=workerFile, path_to_sections=sectionFile)
+        c = Creator.Creator(path_to_workers=WORKER_FILE, path_to_sections=SECTION_FILE)
         c.create_xlsx(".", default_xlsx_path, default_nested_dir)
         c.create_xml(default_xml_path)
         c.create_config_file(default_config_path)
     elif shall_generate_mock == "n" or shall_generate_mock == "N":
+        previous_step_performed = False
         # pass here to cover all other inputs with the else branch
         pass
     else:
@@ -86,7 +108,7 @@ if __name__ == "__main__":
     # will override the old data.xlsx file -> copy it to a new name as backup
     print("Before updating the xlsx-file storing the old file as data.old.xlsx")
     shutil.copy(default_xlsx_path, "data.old.xlsx")
-    FileModifier.XlsxModifier.update_worker_xlsx(worker_cnt, default_xlsx_path)
+    FileModifier.XlsxModifier.update_worker_xlsx(default_worker_cnt, default_xlsx_path)
     manager.generate(tempered_xml)
     differ = XmlDiffer("log/compare_of_updated.log", default_config_path)
     differ.compare(default_xml_path, tempered_xml)
@@ -95,7 +117,7 @@ if __name__ == "__main__":
     print("Step six: add an additional worker")
     print("Creating backup of last xlsx-file under data.updated.xlsx")
     shutil.copy(default_xlsx_path, "data.updated.xlsx")
-    FileModifier.XlsxModifier.add_worker_to_xlsx(worker_cnt, default_xlsx_path)
+    FileModifier.XlsxModifier.add_worker_to_xlsx(default_worker_cnt, default_xlsx_path)
     manager.generate("extended.xml")
     differ = XmlDiffer("log/compare_of_extended.log", default_config_path)
     # test against the updated XML as the xlsx was never reset from the update
